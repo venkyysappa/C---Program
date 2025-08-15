0ee2a45edb3bc4c3670009890dbc14b17b3f66ea
venkyysappa <venkateshvenky6145@gmail.com>
2025-08-15 11:13:41 +0530

    write a program to print greatest of three numbers

---
+++ threenum.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+main()
+{
+	int a,b,c;
+	printf("enter a,b,c");
+	scanf("%d%d%d",&a,&b,&c);
+	if(a>b)
+	printf(" a");
+	else
+	{
+		if(b>c)
+		printf(" b");
+		else
+		printf(" c");
+
+    }
+}
